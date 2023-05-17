454cda1d3448029f119f405be9918b9af1352586
Ploofek <manareyes2004@gmail.com>
2023-05-17 10:47:06 -0700

    VentSent<3

--- app/utils/secrets-temp.py
+++ app/utils/secrets-temp.py
@@ -2,7 +2,7 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':'',
+        'MONGO_HOST':'mongodb+srv://ssofiareyes:drowssap@cluster0.42vojfx.mongodb.net/?retryWrites=true&w=majority',
         'MONGO_DB_NAME':'',
         'GOOGLE_CLIENT_ID': '',
         'GOOGLE_CLIENT_SECRET':'',
